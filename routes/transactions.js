89c4211ec40008a58d42c37b9f30751b3a98dc24
anetaradecka <aneta.radecka@outlook.com>
2023-12-01 15:25:36 +0100

    token authorization to routes added

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,19 +2,25 @@ const express = require("express");
 const { body } = require("express-validator");
 
 const transactionsController = require("../controllers/transactions");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // GET /transactions
-router.get("/", transactionsController.getTransactions);
+router.get("/", isAuth, transactionsController.getTransactions);
 //-------------------------------------------------------------------
 // POST /transactions/add-transaction
 router.post(
   "/add-transaction",
+  isAuth,
   [body("description").trim()],
   transactionsController.postNewTransaction
 );
 // DELETE /:transationId
-router.delete("/:transactionId", transactionsController.deleteTransaction);
+router.delete(
+  "/:transactionId",
+  isAuth,
+  transactionsController.deleteTransaction
+);
 
 module.exports = router;
